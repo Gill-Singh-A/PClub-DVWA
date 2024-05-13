bf0594d3dc674cb690dc66cba6a116db959f21c3
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-13 23:56:54 +0530

    Added Route for Gallery

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python3
 
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+host = "127.0.0.1"
+port = 7000
+debug = True
+
+@app.route("/gallery", methods=["GET"])
+def index():
+    image_files = os.listdir("static/images/gallery")
+    image_data = []
+    for image_file in image_files:
+        image_data.append({"src": f"/static/images/gallery/{image_file}"})
+    return render_template("index.html", images=image_data)
+
 if __name__ == "__main__":
-    pass
+    app.run(host=host, port=port, debug=debug)
