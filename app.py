10b8d972020f5aa9fd5ea458df5df8e6258b86be
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-14 13:11:53 +0530

    Implemented Route for getting Blog Details

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python3
 
-import os
+import os, json
 from flask import Flask, render_template, request
+from mysql.connector import connect, Error
 
 app = Flask(__name__)
 host = "127.0.0.1"
 port = 2324
 debug = True
+
+with open("db_user.json", 'r') as file:
+    login_details = json.load(file)
+database = "pclub_secy_task"
+blog_parts = {"title": 1, "content": 2, "link": 3}
 
 not_allowed_files = ["app.py"]
 
@@ -29,6 +35,19 @@ def getFileRoute():
     with open(file_name, 'rb') as file:
         content = file.read()
     return content, 200
+@app.route("/getBlogDetail", methods=["GET"])
+def getBlogDetailRoute():
+    blog_index = request.args.get("blog")
+    blog_part = request.args.get("part")
+    connection = connect(host="localhost", user=login_details["user"], password=login_details["password"])
+    cursor = connection.cursor()
+    cursor.execute(f"USE {database}")
+    cursor.execute(f"SELECT * FROM BLOGS WHERE id='{blog_index}'")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    result = result[0][blog_parts[blog_part]]
+    return result
 
 if __name__ == "__main__":
     app.run(host=host, port=port, debug=debug)
